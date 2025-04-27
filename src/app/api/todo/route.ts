e0d1f9afb71c623de14ccab207ce6587e2968edd
Himanshu <himanshupoptani12@gmail.com>
2025-04-27 18:28:41 +0530

    fix: now todos can be listed as daily, weekly, or monthly tasks

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -1,4 +1,4 @@
-import { deleteTodo, fetchTodos } from "@/lib/api";
+import { createTodos, deleteTodo, fetchTodos, updateTodo } from "@/lib/api";
 import { auth } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -10,7 +10,7 @@ export async function GET(request: NextRequest) {
         }
 
         const todos = await fetchTodos(userId);
-        return NextResponse.json({ todos: todos }, { status: 200 });
+        return NextResponse.json({ todos }, { status: 200 });
     } catch (error) {
         console.error("Error fetching todos:", error);
         return new NextResponse("Internal Server Error", { status: 500 });
@@ -36,6 +36,57 @@ export async function DELETE(request: NextRequest) {
     }
 }
 
-export async function PATCH(request: NextRequest) {
+export async function POST(request: NextRequest) {
+    try {
+        const { userId } = await auth();
+        if (!userId) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
 
+        const body = await request.json();
+        const { title, frequency, endDate } = body;
+
+        if (!title) {
+            return new NextResponse("Todo title is required", { status: 400 });
+        }
+
+        await createTodos([
+            {
+                title,
+                userId,
+                frequency: frequency ?? "normal",
+                endDate: endDate ? new Date(endDate) : undefined,
+            }
+        ]);
+
+        return new NextResponse("Todo created successfully", { status: 201 });
+    } catch (error) {
+        console.error("Error creating todo:", error);
+        return new NextResponse("Internal Server Error", { status: 500 });
+    }
+}
+
+export async function PATCH(request: NextRequest) {
+    try {
+        const { userId } = await auth();
+        if (!userId) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+
+        const { todoId, completed } = await request.json();
+        if (!todoId) {
+            return new NextResponse("Todo ID is required", { status: 400 });
+        }
+
+        //   await prisma.todo.update({
+        //     where: { id: todoId },
+        //     data: { completed },
+        //   });
+        await updateTodo(todoId, { completed });
+
+        return new NextResponse("Todo updated successfully", { status: 200 });
+    } catch (error) {
+        console.error("Error updating todo:", error);
+        return new NextResponse("Internal Server Error", { status: 500 });
+    }
 }
